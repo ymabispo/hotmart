1f680619b6508e6d95d7313aff8309e8aeb9834e
Yuri Bispo <ymabispo@gmail.com>
2018-07-29 22:32:35 -0300

    arquivo de variaveis terraform

--- 03-provisioning/t2micro-aws.tf
+++ 03-provisioning/t2micro-aws.tf
@@ -1,41 +1,41 @@
 provider "aws" {
   access_key = "xxxx"
   secret_key = "xxxx"
-  region     = "us-east-1"
+  region     = "${var.aws_region}"
 }
 
 resource "aws_instance" "docker_provider" {
-  ami           = "ami-0a2c9ddf8ea042cc1"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_provider}"
+  instance_type = "${var.instance_type}"
   count             = 1
-  key_name      = "personalkey"
+  key_name      = "${var.key_name}"
   security_groups = ["default"]
   tags { Name = "Docker_Provider" }
 }
 
 resource "aws_instance" "docker_manager" {
-  ami           = "ami-0daaf0688c6f6ce2a"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_swarm_manager}"
+  instance_type = "${var.instance_type}"
   count             = 1
-  key_name      = "personalkey"
+  key_name      = "${var.key_name}"
   security_groups = ["default"]
   tags { Name = "Docker_Manager" }
 }
 
 resource "aws_instance" "docker_worker_1" {
-  ami           = "ami-0d6800db6f3005cb4"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_swarm_worker}"
+  instance_type = "${var.instance_type}"
   count             = 1
-  key_name      = "personalkey"
+  key_name      = "${var.key_name}"
   security_groups = ["default"]
   tags { Name = "Docker_Worker_1" }
 }
 
 resource "aws_instance" "docker_worker_2" {
-  ami           = "ami-0a7f2061bb57f0c85"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_swarm_worker}"
+  instance_type = "${var.instance_type}"
   count             = 1
-  key_name      = "personalkey"
+  key_name      = "${var.key_name}"
   security_groups = ["default"]
   tags { Name = "Docker_Worker_2" }
 }
